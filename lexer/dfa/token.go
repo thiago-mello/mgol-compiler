b4d64b53b2ffe8d31f7972ec0126baaaf2a0e9ed
Thiago de Mello <thiagomellojunqueira@gmail.com>
2022-06-12 19:53:56 -0300

    Token data structure

---
+++ lexer/dfa/token.go
@@ -0,0 +1,13 @@
+package dfa
+
+import "fmt"
+
+type Token struct {
+	Class  string
+	Lexeme string
+	Type   string
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("Classe: %s, Lexema: %s, Tipo: %s", t.Class, t.Lexeme, t.Type)
+}
