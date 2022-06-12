68abc05b526829a2e663a74548c8ab1b774cc670
Thiago de Mello <thiagomellojunqueira@gmail.com>
2022-06-12 10:35:00 -0300

    Refactored alphabet map to accept runes

--- lexer/alphabet/alphabet.go
+++ lexer/alphabet/alphabet.go
@@ -1,39 +1,52 @@
 package alphabet
 
-var alphabet = map[string]int{
-	"D":  0,
-	"L":  1,
-	",":  2,
-	";":  3,
-	":":  4,
-	".":  5,
-	"!":  6,
-	"?":  7,
-	"\\": 8,
-	"*":  9,
-	"+":  10,
-	"-":  11,
-	"/":  12,
-	"(":  13,
-	")":  14,
-	"{":  15,
-	"}":  16,
-	"[":  17,
-	"]":  18,
-	"<":  19,
-	">":  20,
-	"=":  21,
-	"\"": 22,
-}
+import "unicode"
 
-func BelongsToAlphabet(char rune) bool {
-	_, ok := alphabet[string(char)]
-
-	return ok
+var alphabet = map[rune]int{
+	'D':  0,
+	'L':  1,
+	'E':  2,
+	'e':  3,
+	',':  4,
+	';':  5,
+	':':  6,
+	'.':  7,
+	'!':  8,
+	'?':  9,
+	'\\': 10,
+	'*':  11,
+	'+':  12,
+	'-':  13,
+	'/':  14,
+	'(':  15,
+	')':  16,
+	'{':  17,
+	'}':  18,
+	'[':  19,
+	']':  20,
+	'<':  21,
+	'>':  22,
+	'=':  23,
+	'"':  24,
+	'_':  25,
 }
 
 func GetRuneIndex(char rune) (int, bool) {
-	index, ok := alphabet[string(char)]
+	char = checkForLetterAndDigit(char)
+	index, ok := alphabet[char]
 
 	return index, ok
+}
+
+func checkForLetterAndDigit(char rune) rune {
+	switch {
+	case char == 'E' || char == 'e':
+		return char
+	case unicode.IsLetter(char):
+		return 'L'
+	case unicode.IsDigit(char):
+		return 'D'
+	default:
+		return char
+	}
 }
