46cbe1abd11d84a3f5d866f13d8eae1373cc8de2
Thiago de Mello <thiagomellojunqueira@gmail.com>
2022-06-13 21:36:19 -0300

    Fixed import cycle

--- symboltable/symbol_table.go
+++ symboltable/symbol_table.go
@@ -1,6 +1,6 @@
 package symboltable
 
-import "github.com/thiago-mello/mgol-compiler/lexer"
+import "github.com/thiago-mello/mgol-compiler/lexer/token"
 
 var reservedWords = [14]string{
 	"inicio",
@@ -19,21 +19,21 @@ var reservedWords = [14]string{
 	"real",
 }
 
-var symbolTable = make(map[string]*lexer.Token, 100) //capacity of 100 to prevent early rehash
+var symbolTable = make(map[string]*token.Token, 100) //capacity of 100 to prevent early rehash
 
 func init() {
 	for _, word := range reservedWords {
-		symbolTable[word] = &lexer.Token{Class: word, Lexeme: word, Type: word}
+		symbolTable[word] = &token.Token{Class: word, Lexeme: word, Type: word}
 	}
 }
 
-func GetElement(lexeme string) (*lexer.Token, bool) {
+func GetElement(lexeme string) (*token.Token, bool) {
 	token, ok := symbolTable[lexeme]
 
 	return token, ok
 }
 
-func AddOrUpdateIfExists(token *lexer.Token) {
+func AddOrUpdateIfExists(token *token.Token) {
 	lexeme := token.Lexeme
 
 	symbolTable[lexeme] = token
