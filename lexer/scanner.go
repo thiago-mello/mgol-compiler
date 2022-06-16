8908fbaf1f0cb62cd4a7564f820327bf3dd52f23
Thiago de Mello <thiagomellojunqueira@gmail.com>
2022-06-15 23:03:05 -0300

    Scanner function to read source file and return a token

---
+++ lexer/scanner.go
@@ -0,0 +1,115 @@
+package lexer
+
+import (
+	"bufio"
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/thiago-mello/mgol-compiler/core"
+	"github.com/thiago-mello/mgol-compiler/lexer/dfa"
+	"github.com/thiago-mello/mgol-compiler/lexer/dfa/classes"
+	"github.com/thiago-mello/mgol-compiler/lexer/e"
+	"github.com/thiago-mello/mgol-compiler/symboltable"
+)
+
+func Scanner(sourceReader *bufio.Reader, row *int, column *int) (core.Token, bool, error) {
+	var (
+		currentState int
+		lexeme       string
+	)
+	for {
+
+		char, _, eofErr := sourceReader.ReadRune()
+		if eofErr != nil && lexeme != "" { //EOF reached and identified lexeme
+			token, ok, err := getToken(currentState, lexeme, *row, *column)
+
+			sourceReader.UnreadRune()
+			return token, ok, err
+		}
+
+		if eofErr != nil {
+			log.Fatal("EOF")
+			return core.Token{Class: "EOF"}, true, e.EndOfFileReachedError(e.END_OF_FILE_REACHED)
+		}
+
+		if isNewline(char) {
+			(*row)++
+			*column = 0
+		}
+
+		tempState, stateTransitionError := dfa.StateTransition(currentState, char)
+
+		if stateTransitionError != nil || tempState < 0 {
+			sourceReader.UnreadRune()
+
+			return getToken(currentState, lexeme, *row, *column)
+		}
+
+		currentState = tempState
+		if currentState != 0 {
+			lexeme += string(char)
+		}
+		(*column)++
+	}
+}
+
+func getToken(currentState int, lexeme string, row int, column int) (core.Token, bool, error) {
+	label, ok := dfa.GetStateLabel(currentState)
+	if !ok {
+		msg := e.GetErrorMsg(e.ERROR_INVALID_WORD, row, column)
+		return core.Token{}, false, errors.New(msg)
+	}
+
+	token, ok := parseState(label, lexeme)
+
+	return token, ok, nil
+}
+
+func parseState(label string, lexeme string) (core.Token, bool) {
+
+	switch label {
+	case classes.IDENTIFICATION_NAME:
+		symbolToken, ok := symboltable.GetElement(lexeme)
+		if !ok {
+			returnToken := &core.Token{
+				Class:  label,
+				Lexeme: lexeme,
+			}
+			symboltable.AddOrUpdateIfExists(returnToken)
+
+			return *returnToken, true
+		}
+
+		return *symbolToken, true
+	case classes.COMMENT:
+		return core.Token{}, false
+	case classes.NUMERIC_CONSTANT_INT, classes.NUMERIC_CONSTANT_FLOAT:
+		labelSplit := strings.Split(label, ".")
+
+		return core.Token{
+			Class:  labelSplit[0],
+			Lexeme: lexeme,
+			Type:   labelSplit[1],
+		}, true
+	case classes.LITERAL_CONSTANT:
+		return core.Token{
+			Class:  label,
+			Lexeme: lexeme,
+			Type:   "literal",
+		}, true
+	default:
+		return core.Token{
+			Class:  label,
+			Lexeme: lexeme,
+		}, true
+	}
+}
+
+func isSpaceOrTab(char rune) bool {
+	return char == ' ' || char == '\t'
+}
+
+func isNewline(char rune) bool {
+	return char == '\n'
+}
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+
+	"github.com/thiago-mello/mgol-compiler/files"
+	"github.com/thiago-mello/mgol-compiler/lexer"
+	"github.com/thiago-mello/mgol-compiler/lexer/e"
 )
 
 func main() {
-	fmt.Println("Welcome to the WIP mgol compiler")
+	row, column := new(int), new(int)
+	var endOfFileReached bool = false
+
+	*row = 1
+	*column = 0
+
+	sourcereader, err := files.SourceReader("./fonte.alg")
+	if err != nil {
+		fmt.Println(errors.New(e.ERROR_OPENING_FILE))
+		return
+	}
+
+	for !endOfFileReached {
+		token, ok, err := lexer.Scanner(sourcereader, row, column)
+		if err != nil {
+			if _, eofReached := err.(e.EndOfFileReachedError); eofReached {
+				eofReached = true
+			} else {
+				fmt.Println(err.Error())
+				continue
+			}
+		}
+
+		if ok {
+			fmt.Println(token.String())
+		}
+	}
 }
