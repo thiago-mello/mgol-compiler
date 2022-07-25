de1614de62f53557f62707b5426fc5c9870acbd9
Thiago de Mello <thiagomellojunqueira@gmail.com>
2022-07-24 23:29:33 -0300

    stack pop multiple implementation

--- parser/stack/stack.go
+++ parser/stack/stack.go
@@ -10,10 +10,25 @@ func (s *Stack) Pop() (int, bool) {
 	if len(*s) == 0 {
 		return 0, false
 	}
-
 	index := len(*s) - 1
 	pop := (*s)[index]
 	*s = (*s)[:index]
+
+	return pop, true
+}
+
+func (s *Stack) PopMultiple(n int) ([]int, bool) {
+	if len(*s) == 0 || len(*s) < n {
+		return nil, false
+	}
+
+	pop := make([]int, 25)
+	index := len(*s) - 1
+
+	for i := 0; i < n; i++ {
+		pop = append(pop, (*s)[index-i])
+		*s = (*s)[:index-i]
+	}
 
 	return pop, true
 }
